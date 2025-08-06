51e48f79f6fc5ad5afe434da80bf3bc577f5f2a6
tadala2005 <tadalachingeni@gmail.com>
2025-08-06 10:05:13 +0200

    question 14

---
+++ Q14.js
@@ -0,0 +1,8 @@
+
+
+setTimeout(() => {
+    const color = document.getElementById("one");
+    color.innerHTML = #fffff;
+
+}
+, 2000);.
---
+++ Q15.js
@@ -0,0 +1,6 @@
+const head = document.getElementById("head")
+const par = document.getElementById("par")
+
+head.textContent = "this is the header";
+par.textContent ="this is a paragraph"
+
