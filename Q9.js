37c9ce0971b086430c5f8940f09dc52702af31a8
tadala2005 <tadalachingeni@gmail.com>
2025-08-06 08:58:49 +0200

    question 9

---
+++ Q9.js
@@ -0,0 +1,12 @@
+
+function numbers(numbers =[]){
+    let average = 0;
+    for(let i =0;i<numbers.length;i++){
+         return Math.min(numbers);
+        
+    }
+    average = numbers
+    console.log(average)
+
+}
+
