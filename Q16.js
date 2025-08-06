2bf0e78b53d9278279cb8bb02a6273ee3838dd53
tadala2005 <tadalachingeni@gmail.com>
2025-08-06 10:18:24 +0200

    question 15

--- Q16.js
+++ Q16.js
@@ -1,5 +1,4 @@
 
-    <script>
         const products = [
           { title: "Wireless Headphones", price: 59.99 },
           { title: "Electric Kettle", price: 29.99 },
@@ -11,22 +10,20 @@
 
         // Loop through each element in the array
         products.forEach(product => {
-          // Create a div element
+          
           const div = document.createElement("div");
 
-          // Creates an <h1> element to contain the title
           const h1 = document.createElement("h1");
           h1.textContent = product.title;
 
-          // Creates p element to contain the price
           const p = document.createElement("p");
           p.textContent = Price: ${product.price};
 
-          // Appends the h1 and the p elements to the div
+      
           div.appendChild(h1);
           div.appendChild(p);
 
-          // Appends the div to the body element
+          /
           document.body.appendChild(div);
         });
-    </script>
+   
