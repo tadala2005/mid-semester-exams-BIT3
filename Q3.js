fcfa7900a7d8fc6531291d31660ea4ac1f1b7d34
tadala2005 <tadalachingeni@gmail.com>
2025-08-06 08:02:20 +0200

    question 3

---
+++ Q3.js
@@ -0,0 +1,29 @@
+//division
+let a = 5;
+let b = 6;
+
+let quatient = a/b;
+console.log(quatient);
+
+//logical NOT
+if(a ===! 5){
+    console.log(`"a" is not equall to 5`)
+}
+else{
+    console.log(quatient)
+}
+
+//ternery operator
+let ishungry = true;
+let food= ishungry ? "give them food":"do not give them food"
+
+
+//incremen and decrement operators 
+
+for(let i= 0; i<10; i++){
+    console.log(i)
+}
+
+for(let i= 10; i>0; i--){
+    console.log(i)
+}
