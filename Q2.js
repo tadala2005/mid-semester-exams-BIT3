2d9ab61ecb50dbc61cc5290606f9b53b306d31c8
tadala2005 <tadalachingeni@gmail.com>
2025-08-06 07:53:19 +0200

    question 2

---
+++ Q2.js
@@ -0,0 +1,12 @@
+//var is used just to declare a variable , it is not possible reassign it again
+var name = "tadala";
+name ="craig"
+
+let name2 = "tadala"
+name2 = "craig";
+// let is used to declare varaiblees than can be alterd
+
+const home ="blantyre";
+home = "blantyre";
+// const is used to declare variables that do not change
+
